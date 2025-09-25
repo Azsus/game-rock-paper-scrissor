b846f3f1011271e7f5ac3f4e12be124ec4715eba
Azsus <nirastisai8@gmail.com>
2025-09-26 00:05:18 +0700

    <MESSAGE>

--- index.js
+++ index.js
@@ -1,17 +1,66 @@
-function playerinput(){
-
+function Userinput() {
+    userInput = document.getElementById("user-input").value
+    console.log(userInput)
 
 }
 
-function botinput(){ 
-
+function BotInput(){ 
+   botInput = Math.floor((Math.random() * 100) %3)
+   console.log(botInput)
 }
 
 function comparebotandplayer(){ 
+    //rock :0
+    //paper :2
+    //sissor :2
+   if (userInput == botInput){
+      console.log("DRAW")
 
-}   
+   } else if (userInput == 0 && botInput == 2){
+      console.log ("YOUWIN")
 
-function Displayresult() {
+   } else if (userInput == 1 && botInput == 0){
+      console.log ("YOUWIN")
+
+   } else if (userInput == 2 && botInput == 1){
+      console.log ("YOUWIN")
+
+   } else {
+      console.log("YOU LOSE")
+   }    
+
+}
+
+function PlayGame() {
+    userInput = document.getElementById("user-input").value
+    console.log(userInput)
+
+    botInput = Math.floor((Math.random() * 100) %3)
+    console.log(botInput)
+
+     if (userInput == botInput){
+      console.log("DRAW")
+
+   } else if (userInput == 0 && botInput == 2){
+      console.log ("YOUWIN")
+
+   } else if (userInput == 1 && botInput == 0){
+      console.log ("YOUWIN")
+
+   } else if (userInput == 2 && botInput == 1){
+      console.log ("YOUWIN")
+
+   } else {
+      console.log("YOU LOSE")
+
+}
+
+
+
+
+
+
+
 
 }
     
